2e1cbe926cf8ce2addc8b0823c32675d2190a3a7
Ashika-Siriwardhan <ashikadhh@gmail.com>
2023-09-17 11:01:59 +0530

    first commit

---
+++ src/cmjd/pkg103/assignment/pos_layered/Cmjd103AssignmentPos_layered.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cmjd.pkg103.assignment.pos_layered;
+
+/**
+ *
+ * @author user
+ */
+public class Cmjd103AssignmentPos_layered {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
